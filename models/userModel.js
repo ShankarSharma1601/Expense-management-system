3e7e83b20b09ecdc024cad3d8f5909aca38413c1
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-01-01 18:04:05 +0530

    User model design

---
+++ models/userModel.js
@@ -0,0 +1,25 @@
+const { timeStamp } = require("console");
+const mongoose = require("mongoose");
+
+//schema design
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "email is required and should be unique"],
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+//export
+const userModel = mongoose.model("users", userSchema);
+module.exports = userModel;
